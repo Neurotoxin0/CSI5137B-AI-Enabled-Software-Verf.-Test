81956b6d35dfd2b8713f59edb52a715773cfde63
Neurotoxin0 <Xusi101066@163.com>
2024-10-01 19:39:23 -0400

    Add euclidean_distance method

--- A1/tsp_solver.py
+++ A1/tsp_solver.py
@@ -5,15 +5,59 @@
 @Comment      :   Dev with Python 3.10.0
 """
 
+import math, random
+
+
+def euclidean_distance(city1: tuple, city2: tuple) -> float:
+    """
+    Calculate the Euclidean distance between two cities.
+
+    Parameters:
+    - city1 (tuple): A tuple containing the x and y coordinates of the first city.
+    - city2 (tuple): A tuple containing the x and y coordinates of the second city.
+
+    Returns:
+    - distance (float): The Euclidean distance between the two cities.
+    """
+    return math.sqrt((city1[0] - city2[0]) ** 2 + (city1[1] - city2[1]) ** 2)
+
 
 class GeneticAlgorithm:
-    def __init__(self, tsp_file):
-        self.tsp_file = tsp_file
+    """
+    A class to represent a genetic algorithm solver for the Travelling Salesman Problem (TSP).
+
+    Attributes:
+    #TODO
+
+    Methods:
+    #TODO
+    """
+
+    def __init__(self, popsize: int, mutation_rate: float = 0.1, generations: int = 1000) -> None:
+        """
+        Initialize the genetic algorithm solver with the given parameters.
+
+        Parameters:
+        - popsize (int): The size of the population.
+        - mutation_rate (float): The probability of a mutation occurring during crossover.
+        - generations (int): The number of generations to run the genetic algorithm.
+
+        Returns:
+        - None
+        """
+        self.popsize = popsize
+        self.mutation_rate = mutation_rate
+        self.generations = generations
         self.population = []
         self.best_individual = None
-        self.best_cost = None
+        self.cities = []
+
 
     def solve(self):
+        pass
+
+
+    def assess_fitness(self):
         pass
 
     def initialize_population(self):
