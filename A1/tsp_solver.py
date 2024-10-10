786d4076c486ad207daeed5dd27213911f3e7c30
Neurotoxin0 <Xusi101066@163.com>
2024-10-10 15:03:14 -0400

    Minor

--- A1/tsp_solver.py
+++ A1/tsp_solver.py
@@ -10,7 +10,7 @@ from concurrent.futures import ProcessPoolExecutor, as_completed
 
 
 Path = (os.path.split(os.path.realpath(__file__))[0] + "/").replace("\\\\", "/").replace("\\", "/")
-debug = True
+debug = False
 
 
 def euclidean_distance(city1: tuple, city2: tuple) -> float:
