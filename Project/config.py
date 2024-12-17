4441009dd6f06d7f695714dfb0c1e8cfeac3913c
Neurotoxin0 <Xusi101066@163.com>
2024-12-16 20:04:03 -0500

    Minor

--- Project/config.py
+++ Project/config.py
@@ -2,6 +2,7 @@
 debug = True
 iterations = 1000
 max_workers = 25     # Maximum number of workers for concurrent processing
+load_existing_delivery_problem = True   # Load existing DeliveryProblem instance from file if True, else create a new instance
 
 distance_scale = 0.001   # Convert distance (distance.csv) from meters to kilometers
 weight_scale = 0.0001    # Convert weight (order_small.csv / order_large.csv) from 0.1 grams to kilograms
